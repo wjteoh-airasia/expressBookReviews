2aa9956f6ea9870c3522c3765f052e228d3d2863
SmithsWaywardHome <collin_smith@icloud.com>
2024-04-25 03:35:22 +0000

    step 3 commit 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,20 +19,22 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  let isbn = req.params.isbn;
-  res.send(JSON.stringify(books[isbn],null,4));
+  let param = req.params.isbn;
+  res.send(JSON.stringify(books[param],null,4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let param = req.params.author;
+  res.send(JSON.stringify(books[param],null,4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let param = req.params.title;
+  res.send(JSON.stringify(books[param],null,4));
 });
 
 //  Get book review
