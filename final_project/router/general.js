3418f28206ef8cb77da7f5d67fa8a72795de1d59
SmithsWaywardHome <collin_smith@icloud.com>
2024-04-25 20:30:06 +0000

    task 5 final commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -41,7 +41,7 @@ public_users.get('/title/:title',function (req, res) {
   const param = req.params.title;
   let matches = {};
   for (const [key, value] of Object.entries(books)) {
-    if(value.author === param)
+    if(value.title === param)
       matches[key] = value;
   }
   res.send(JSON.stringify(matches,null,4));
@@ -50,7 +50,8 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const param = req.params.isbn;
+  res.send(JSON.stringify(books[param].reviews,null,4));
 });
 
 module.exports.general = public_users;
