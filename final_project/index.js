833ca309d794a68c9812deff09cc14ef22ef07f6
SmithsWaywardHome <collin_smith@icloud.com>
2024-04-25 03:22:19 +0000

    step 2 commit

--- final_project/index.js
+++ final_project/index.js
@@ -28,7 +28,7 @@ app.use("/customer/auth/*", function auth(req,res,next){
     }
 });
  
-const PORT =5000;
+const PORT =8081;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,13 +13,14 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn],null,4));
  });
   
 // Get book details based on author
