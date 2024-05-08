ac91474c39a5e8298c25856350005ae686163155
J-BitByte <j_a_loubert@hotmail.com>
2024-05-08 05:00:50 -0400

    changes to index.js

--- final_project/index.js
+++ final_project/index.js
@@ -3,7 +3,16 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-
+const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 const app = express();
 
 app.use(express.json());
@@ -11,7 +20,24 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        return res.status(404).json({message:"Error Logging In"});
+
+    }   
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', {expiresIn: 60*60});
+        req.session.authorization = {
+            accessToken,username
+        } 
+        return res.status(200).send("User Successfully Logged In");       
+    } else {
+        return res.status(208).json({message: "Invalid Login. Check username and password."});
+    }    
 });
  
 const PORT =5000;
