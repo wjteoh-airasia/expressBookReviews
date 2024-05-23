faa36270fd5ed4a3e30856e0e853879409633850
Eben <kwabenat679@gmail.com>
2024-05-23 16:21:29 +0000

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,17 @@
-let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
-
-module.exports=books;
+let books = [ 
+    
+      {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": 'Epic book', "ISBN": "10212332"},
+      {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": "Boring book💤🥱, wouldn't read twice" , "ISBN": "10212333"},
+      {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": 'Not bad', "ISBN": "10212334"},
+      {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": 'Good book', "ISBN": "10212335"},
+      {"author": "Unknown","title": "The Book Of Job", "reviews": 'Interesting story', "ISBN": "10212336"},
+      {"author": "Unknown","title": "One Thousand and One Nights", "reviews": 'I love it', "ISBN": "10212337"},
+      {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": 'Are they releasing new ones?', "ISBN": "10212338"},
+      {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": 'Not that bad'  , "ISBN": "10212339"},
+      {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": 'Wow!!😍 Amazing book', "ISBN": "10212340"},
+       {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": 'Terrible, I wasted my time😔', "ISBN": "10212341"}
+  
+  ]
+  
+  module.exports=books;
+  
