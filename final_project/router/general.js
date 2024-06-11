3043b81e995af46d796c8fedb000dfe91c9e518d
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-06-11 12:05:48 -0400

    Register users

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,9 +5,20 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+//Register users by username and password
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password){
+    if (!doesExist(username)){
+      user.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registered. Now you can login"});
+    } else {
+      return res.status(200).json({message:"User already exists"});
+    }  
+  }
+  return res.status(404).json({message:"Unable to register user."})
 });
 
 // Get the book list available in the shop
