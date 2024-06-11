c0d1c452502fa5b6fd04580fdc114e5b88f65423
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-06-11 10:40:57 -0400

    Get Author-details

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,8 +39,23 @@ public_users.get('/isbn/:isbn',function (req, res){
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  const author = req.params.author;
+  let book;
+  
+  for(let key in books){
+    if(books[key].author === author){
+       book = books[key];
+       break;
+    }
+   }
+
+   if (book){
+       res.json(book);
+   }  else{
+        res.status(404).json({message: "Book not found"})
+   }
+  
 });
 
 // Get all books based on title
