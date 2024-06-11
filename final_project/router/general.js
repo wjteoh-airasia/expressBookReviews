014a17c425c506f43bd8e73222e8101e3688f6d9
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-06-11 07:25:31 -0400

    New code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,16 +19,19 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  
-  const isbn = req.params.isbn;
-  const book = books[isbn];
 
-  if (book) {
-    res.json(book);
-  } else {
-    res.status(404).json({message: "Book not found"});
-  }
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+
+    if (book) {
+     res.json(books);
+    } else if (isbn) {
+        res.json(isbn);
+    } else {
+      res.status(404).json({message: "Book not found"});
+    }
 });
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
