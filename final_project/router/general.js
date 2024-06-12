e8123b2bc724c629268ae60662797a60c62d183a
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-06-12 03:37:46 -0400

    Add potential fix

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,16 +11,15 @@ public_users.post("/register", (req,res) => {
   const password = req.body.password;
 
   if (username && password){
-      if(!doesExist(username)){
+    if(isValid(username)) {
+      return res.status(400).json({message:"User already exists"});
+    } else {
       users.push({"username":username,"password":password});
       return res.status(200).json({message: "User successfully registered. Now you can login"});
-    } else {
-      return res.status(404).json({message:"User already exists"});
-     }
-    }  
-  return res.status(404).json({message:"Unable to register user."});
-  });
-  
+    }
+   }
+   return res.status(404).json({message:"Unable to register user"});
+});
 
 //Checks if username does exists
 const doesExist = (username)=> {
