989cc6ae2122a228031a0d3acc2d984ce2c5b100
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-06-12 01:52:16 -0400

    Add isValdi function

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,12 +10,16 @@ public_users.post("/register", (req,res) => {
   const password = req.body.password;
 
   if (username && password){
-    if (!doesExist(username)){
+    if (!isValid(username)) {
+      if(!doesExist(username)){
       users.push({"username":username,"password":password});
       return res.status(200).json({message: "User successfully registered. Now you can login"});
     } else {
-      return res.status(404).json({message:"User already exists"});
+      return res.status(400).json({message:"User already exists"});
     }  
+  } else {
+    return res.status(400).json({message:"Invalid username"})
+   }
   }
   return res.status(404).json({message:"Unable to register user."});
 });
