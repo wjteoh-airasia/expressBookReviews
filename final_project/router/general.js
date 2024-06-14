1b864c171f9d66e9dbc5bfb9f7bb905f783f973b
J-BitByte <j_a_loubert@hotmail.com>
2024-06-13 20:41:45 -0400

    update from lab

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,20 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   res.send(JSON.stringify(books,null,4));
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+    res.send(books[isbn])
+  //return res.status(300).json({message: "ISBN"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "Author"});
 });
 
 // Get all books based on title
