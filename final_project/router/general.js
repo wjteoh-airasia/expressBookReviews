df122592f38d2c140c99790f7788aac5c37d81db
jjchiramal <jjchiramal@gmail.com>
2024-07-02 13:34:01 -0400

    Updated teh code in index.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,19 +12,27 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
+  res.send(JSON.stringify({books}, null, 4));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+    
+    // Extract the author parameter from the request URL
+    const author1 = req.params.author;
+    // Filter the books array to find books which author matches the extracted author parameter
+    //let filtered_books = books.filter((author1) => books.author === author1);
+    // Send the filtered_users array as the response to the client
+    res.send(filtered_books);
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
