dfcbd8cb3ed68f370c5a9cdf054b14f9e1b044c4
J-BitByte <157428550+J-BitByte@users.noreply.github.com>
2024-07-08 15:21:29 +1000

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -3,16 +3,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-const authenticatedUser = (username,password)=>{
-    let validusers = users.filter((user)=>{
-      return (user.username === username && user.password === password)
-    });
-    if(validusers.length > 0){
-      return true;
-    } else {
-      return false;
-    }
-  }
+
 const app = express();
 
 app.use(express.json());
@@ -20,26 +11,22 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-    const username = req.body.username;
-    const password = req.body.password;
-
-    if (!username || !password) {
-        return res.status(404).json({message:"Error Logging In"});
-
-    }   
-    if (authenticatedUser(username, password)) {
-        let accessToken = jwt.sign({
-            data: password
-        }, 'access', {expiresIn: 60*60});
-        req.session.authorization = {
-            accessToken,username
-        } 
-        return res.status(200).send("User Successfully Logged In");       
+ if(req.session.authorization) {
+       token = req.session.authorization['accessToken'];
+       jwt.verify(token, "access",(err,user)=>{
+           if(!err){
+               req.user = user;
+               next();
+           }
+           else{
+               return res.status(403).json({message: "User not authenticated"})
+           }
+        });
     } else {
-        return res.status(208).json({message: "Invalid Login. Check username and password."});
-    }    
+        return res.status(403).json({message: "User not logged in"})
+    }
 });
- 
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
