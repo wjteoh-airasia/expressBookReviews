bc7f3949d0ad281a6dbee23a9496dfc2deecd2fd
J-BitByte <157428550+J-BitByte@users.noreply.github.com>
2024-07-08 13:29:20 +1000

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -26,7 +26,29 @@ if(isValid(username)){
     
 
 }
-
+regd_users.post("/register", (req,res) => {
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+    if(username&&password){
+        const present = users.filter((user)=> user.username === username)
+        if(present.length===0){
+            users.push({"username":req.body.username,"password":req.body.password});
+            return res.status(201).json({message:"USer Created successfully"})
+        }
+        else{
+          return res.status(400).json({message:"Already exists"})
+        }
+    }
+    else if(!username && !password){
+      return res.status(400).json({message:"Bad request"})
+    }
+    else if(!username || !password){
+      return res.status(400).json({message:"Check username and password"})
+    }
+  
+   
+  });
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
