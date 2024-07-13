2b5f4d7b335b065e41f90050a72e29429cb62841
edharad <egzon.d.haradinaj@gmail.com>
2024-07-13 08:54:59 -0400

    changes are done for the final project

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,27 +3,113 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [{username: "edharad", password: "pwd12"}];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+
+    if (userswithsamename.length > 0) {
+        return true;
+        } else {
+         return false;
+        }
+    //write code to check is the username is valid
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
+
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 //write code to check if username and password match the one we have in records.
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.authorization.username; 
+
+    if (!username) {
+      return res.status(401).json({ message: "Unauthorized" }); 
+    }
+
+    const book = books[isbn];
+
+    if (book) {
+      book.reviews[username] = review; 
+      res.json({ message: "Review added/modified successfully" });
+    } else {
+      res.status(404).json({ message: "Book not found" }); // Handle book not found
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error adding/modifying review" }); // Handle unexpected errors
+  }
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => { 
+    try {
+        const isbn = req.params.isbn;
+        const review = req.query.review;
+        const username = req.session.authorization.username; 
+    
+        if (!username) {
+          return res.status(401).json({ message: "Unauthorized" }); 
+        }
+    
+        const book = books[isbn];
+    
+        if (book) {
+          delete book.reviews[username] ; 
+          res.json({ message: "Review deleted successfully" });
+        } else {
+          res.status(404).json({ message: "Book not found" }); // Handle book not found
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error deleting review" }); // Handle unexpected errors
+      }
+    
+})
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
