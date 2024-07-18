80c70212f8b3f2a53809a6e48af5e6be4ca31c89
gijoharji <gijoharji@gmail.com>
2024-07-18 14:58:28 +0300

    Create booksdb.js

---
+++ final_project/booksdb.js
@@ -0,0 +1,14 @@
+let books = {
+  "1": { "isbn": "978-3-16-148410-0", "author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": {} },
+  "2": { "isbn": "978-1-23-456789-0", "author": "Hans Christian Andersen", "title": "Fairy tales", "reviews": {} },
+  "3": { "isbn": "978-1-40-289462-6", "author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": {} },
+  "4": { "isbn": "978-0-19-283547-4", "author": "Unknown", "title": "The Epic Of Gilgamesh", "reviews": {} },
+  "5": { "isbn": "978-0-14-044914-3", "author": "Unknown", "title": "The Book Of Job", "reviews": {} },
+  "6": { "isbn": "978-0-19-283746-1", "author": "Unknown", "title": "One Thousand and One Nights", "reviews": {} },
+  "7": { "isbn": "978-0-14-044769-9", "author": "Unknown", "title": "Njál's Saga", "reviews": {} },
+  "8": { "isbn": "978-0-19-283355-5", "author": "Jane Austen", "title": "Pride and Prejudice", "reviews": {} },
+  "9": { "isbn": "978-0-19-283356-2", "author": "Honoré de Balzac", "title": "Le Père Goriot", "reviews": {} },
+  "10": { "isbn": "978-0-19-283353-1", "author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+};
+
+module.exports = books;
