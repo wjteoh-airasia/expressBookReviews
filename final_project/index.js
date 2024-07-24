791110e9c2ff56571511f488f2b489f21eddbed2
pankaj kumar <pankajuday7033@gmail.com>
2024-07-24 22:19:15 +0530

    first task completed "add  authenication mechanism"

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,18 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+if(req.session.authenticated){
+    let token = req.session.authenticated['accessToken'];
+
+    jwt.verify(token,'access',(err,user)=>{
+        if(!err){
+            req.user = user;
+            next();
+        }else{
+            return res.status(403).json({massage:'user not authenticated'})
+        }
+    })
+}
 });
  
 const PORT =5000;
