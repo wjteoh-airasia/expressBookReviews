0b2ed57035c9b6f660de72ac06a9617c5bb8c0ec
Jayden-Srm <jaydenseeram2005@gmail.com>
2024-07-25 02:42:12 -0400

    Add files via upload

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,15 @@
-let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
-
-module.exports=books;
+const books = [
+      { "id": 1, "isbn": "0-02-044520-0", "author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": 5 },
+      { "id": 2, "isbn": "0-553-21311-4", "author": "Hans Christian Andersen", "title": "Fairy Tales", "reviews": 5 },
+      { "id": 3, "isbn": "0-19-281265-2", "author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": 3 },
+      { "id": 4, "isbn": "0-14-044076-4", "author": "Unknown", "title": "The Epic Of Gilgamesh", "reviews": 2 },
+      { "id": 5, "isbn": "0-14-044104-3", "author": "Unknown", "title": "The Book Of Job", "reviews": 1 },
+      { "id": 6, "isbn": "0-14-044299-5", "author": "Unknown", "title": "One Thousand and One Nights", "reviews": 4 },
+      { "id": 7, "isbn": "0-19-955596-3", "author": "Unknown", "title": "Njáls Saga", "reviews": 5 },
+      { "id": 8, "isbn": "0-14-143951-2", "author": "Jane Austen", "title": "Pride and Prejudice", "reviews": 3 },
+      { "id": 9, "isbn": "0-14-044015-5", "author": "Honoré de Balzac", "title": "Le Père Goriot", "reviews": 2 },
+      { "id": 10, "isbn": "0-67-972874-6", "author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": 5 }
+    ];
+    
+    module.exports = books;
+    
