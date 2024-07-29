e73b85acdb0d0660c3a6bf9794e6039b8fcefbde
Ali <122575863+alimbafageeh@users.noreply.github.com>
2024-07-29 06:11:24 +0300

    Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -51,7 +51,20 @@ public_users.get('/author/:author', function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  let booksByTitle = [];
+
+  for (let key in books) {
+    if (books[key].title.toLowerCase() === title.toLowerCase()) {
+      booksByTitle.push(books[key]);
+    }
+  }
+
+  if (booksByTitle.length > 0) {
+    return res.status(200).json(booksByTitle);
+  } else {
+    return res.status(404).json({ message: "Books with this title not found" });
+  }
 });
 
 //  Get book review
