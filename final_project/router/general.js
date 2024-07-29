aceb1ecd4d7f0f465555820ea1fb039f265cf5e8
Ali <122575863+alimbafageeh@users.noreply.github.com>
2024-07-29 06:08:13 +0300

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,6 +18,7 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
+  //Write your code here
   const isbn = req.params.isbn;
   const book = books[isbn];
 
@@ -31,7 +32,20 @@ public_users.get('/isbn/:isbn', function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author;
+  let booksByAuthor = [];
+
+  for (let key in books) {
+    if (books[key].author === author) {
+      booksByAuthor.push(books[key]);
+    }
+  }
+
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor);
+  } else {
+    return res.status(404).json({ message: "Books by this author not found" });
+  }
 });
 
 // Get all books based on title
