cea2a23ca759280576fb5f6e6c2cf70d244b67fc
Ali <122575863+alimbafageeh@users.noreply.github.com>
2024-07-29 07:37:09 +0300

    Task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -68,6 +68,27 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   }
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization?.username;
+
+  if (!username) {
+    return res.status(403).json({ message: "User not logged in" });
+  }
+
+  if (books[isbn]) {
+    if (books[isbn].reviews[username]) {
+      delete books[isbn].reviews[username];
+      return res.status(200).json({ message: "Review deleted successfully" });
+    } else {
+      return res.status(404).json({ message: "Review not found for this user" });
+    }
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,13 +25,13 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
+public_users.get('/', (req, res) => {
   //Write your code here
   return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', (req, res) => {
   //Write your code here
   const isbn = req.params.isbn;
   const book = books[isbn];
@@ -44,7 +44,7 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
 
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', (req, res) => {
   //Write your code here
   const author = req.params.author;
   let booksByAuthor = [];
@@ -63,7 +63,7 @@ public_users.get('/author/:author', function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', (req, res) => {
   //Write your code here
   const title = req.params.title;
   let booksByTitle = [];
@@ -82,7 +82,7 @@ public_users.get('/title/:title', function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn', function (req, res) {
+public_users.get('/review/:isbn', (req, res) => {
   //Write your code here
   const isbn = req.params.isbn;
   const book = books[isbn];
