4152f20b7fb34d7224f406b15357089d3ad37c37
cagriefe <cagriefe34@gmail.com>
2024-08-09 12:58:56 +0300

    book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,10 +16,16 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  
+  if (book) {
+    return res.status(200).json(book);
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
