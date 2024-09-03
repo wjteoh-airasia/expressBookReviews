edc4ee4ed556fed9ecd748d0137bad1257b48c0c
pankaj <pankajuday7033@gmail.com>
2024-09-03 23:22:35 +0530

    feat: Complete server-side online book review application
    - Built a RESTful API for the online book review application
    - Integrated JWT-based session authentication for secure user access
    - Implemented core functionality, including book reviews and user authentication and authorization mechanisms
    - Utilized Promises, callbacks functions for handling asynchronous operations
    - Thoroughly tested all endpoints using Postman to ensure functionality and security

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,49 +1,22 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
-const crypto = require('crypto');
-
+const crypto = require("crypto");
 
 let users = [];
 
-const session_secret = crypto.randomBytes(64).toString('hex');
+const login_session_secret = crypto.randomBytes(64).toString("hex");
 
-const isValid = (username) => { //returns boolean
-  //write code to check is the username is valid
-  const v_user = users.filter((user) => {
-    return user.username == username;
-
-  })
-  if (v_user.length > 0) {
-    return true;
-  } else {
-    return false;
-  }
+const isValid = (username) => {
+  return users.some((user) => user.username === username);
 };
 
-const authenticatedUser = (username, password) => { //returns boolean
-  //write code to check if username and password match the one we have in records.
-  const auth_user = users.filter((user) => {
-    return (user.username == username && user.password == password);
-
-  });
-  if (auth_user.length > 0) {
-    return true;
-  } else { 
-    return false; 
-  }
+const authenticatedUser = (username, password) => {
+  return users.some(
+    (user) => user.username === username && user.password === password
+  );
 };
-const add = ()=>{
-
-};
-
-const modify =()=>{
-
-};
-
-
-
 
 //only registered users can login
 regd_users.post("/login", (req, res) => {
@@ -53,45 +26,80 @@ regd_users.post("/login", (req, res) => {
   const password = req.body.password;
 
   if (!username || !password) {
-    return res.status(404).send("Error logging in")
+    return res.status(404).send("Error logging in");
   }
   if (authenticatedUser(username, password)) {
-
     // Generate token
-    let JW_Token = jwt.sign({
-      data: password
-    }, session_secret, { expiresIn: 60 * 60 });
+    let JW_Token = jwt.sign(
+      {
+        data: password,
+      },
+      login_session_secret,
+      { expiresIn: 60 * 60 }
+    );
 
     //Store Access Token and username in Session
     req.session.authorization = {
-      JW_Token, username
+      JW_Token,
+      username,
     };
     return res.status(200).send("User successfully logged in");
   } else {
-    return res.status(208).json({ message: "Invalid Login: Please Check username and password" });
+    return res
+      .status(208)
+      .json({ message: "Invalid Login: Please Check username and password" });
   }
-
 });
 
-// Add a book review
+// Add and Modify a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  const isbn = req.params.isbn;
-  const book = books[isbn];
-  if(book){
-    let review = req.body.reviews;
-    if(review){
-      book['reviews'] = review;
-    }
-    books[isbn] = book;
-    return res.send(`Book with the ISBN ${isbn} Updated reviews`)
-  }else{
-   return res.send(`Unable to find book with ISBN ${isbn}`);
+  if (!req.session.authorization) {
+    res.status(403).json({ message: "User not authenticated" });
   }
-  
+  const { isbn } = req.params;
+  const { review } = req.body;
 
+  if (!isbn && !review && review !== null) {
+    return res
+      .status(400)
+      .json({ message: "Invalid input: ISBN and review should be required" });
+  }
+
+  if (books[isbn]) {
+    const book = books[isbn];
+    const getReviewObj = book["reviews"];
+    const username = req.session.authorization.username;
+    if (getReviewObj[username]) {
+      getReviewObj[username] = { review: review };
+      res.status(200).send(`Review modified successfully ISBN : ${isbn}`);
+    } else {
+      getReviewObj[username] = { review: review };
+      res.status(200).send(`Review added successfully ISBN : ${isbn}`);
+    }
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const { isbn } = req.params;
+  const username = req.session.authorization.username;
+
+  if (books[isbn]) {
+    const book = books[isbn];
+    const review = book["reviews"];
+    if (review[username]) {
+      delete review[username];
+      res.status(200).send("Review deleted successfully ");
+    } else {
+      res.status(404).send("Review not found");
+    }
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+module.exports.logSec = login_session_secret;
