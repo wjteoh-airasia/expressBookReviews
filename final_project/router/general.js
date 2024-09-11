1d6c0715e6cd40ee82b83d52ba6ab9a2b610c8af
Letlhogonolo Chauke <PITROSCHAUKE24@GMAIL.COM>
2024-09-11 08:58:08 +0200

    Task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -63,7 +63,15 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
+
+  const isbn = req.params.isbn;
+
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn].reviews)
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
