ebd3f73da0e81d94ff2f9834fc441aa5a2595aa2
oubai saker <oubaisaker555@gmail.com>
2024-09-12 13:04:06 +0300

    git book details

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,8 +21,12 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  return res.status(300).json({
+    message: "Yet to be implemented",
+    book: book
+  });
 });
 
 // Get book details based on author
