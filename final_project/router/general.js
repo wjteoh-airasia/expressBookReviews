08dd08d154850617f6eace6ebf5c28645d74007f
AbdallaRabeaMabed <abdallamabed407@gmail.com>
2024-09-21 20:52:11 -0400

    Task6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,9 +5,26 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+public_users.post("/register", (req, res) => {
+    const { username, password } = req.body;
+
+    // Check if username and password are provided
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Username and password are required' });
+    }
+
+    // Check if user already exists
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+        return res.status(400).json({ message: 'Username already exists' });
+    }
+
+    // Create new user
+    const newUser = { username, password };
+    users.push(newUser);
+
+    return res.status(201).json({ message: 'User registered successfully' });
 });
 
 // Get the book list available in the shop
