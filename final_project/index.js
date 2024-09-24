edb55f56869bacd9626dabb4ccbef97b98de28ec
Swapnil Pawar <swapnil.pawar9870@gmail.com>
2024-09-24 09:42:17 -0400

    task 8 working

--- final_project/index.js
+++ final_project/index.js
@@ -14,6 +14,19 @@ app.use("/customer", session({ secret: process.env.SECRET_KEY, resave: true, sav
 
 app.use("/customer/auth/*", function auth(req, res, next) {
     //Write the authenication mechanism here
+    const token = req.headers["authorazation"];
+
+    if (!token) {
+        return res.status(403).json({ message: "Access denied. No token provided." });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);  // Verify the token using the secret key
+        req.user = decoded;  // Attach the decoded user information to the request object
+        next();  // Pass control to the next middleware or route handler
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid token." });
+    }
 });
 
 const PORT = 5000;
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -23,14 +23,51 @@ const authenticatedUser = (username, password) => {
 
 //only registered users can login
 regd_users.post("/login", (req, res) => {
-    //Write your code here
-    return res.status(300).json({ message: "login" });
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-    //Write your code here
-    return res.status(300).json({ message: "auth/review" });
+    console.log("Received PUT request for ISBN:" + req.params.isbn);
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.username;
+
+    if (!review || !username) {
+        return res.status(400).json({ message: "Review text and username must be provided." });
+    }
+
+    if (!books[isbn]) {
+        return res.status(404).json({ message: "Book not found." });
+    }
+
+    if (!books[isbn].reviews) {
+        books[isbn].reviews = {};
+    }
+
+    books[isbn].reviews[username] = review;
+    // books[isbn].reviews[username] = username;
+    // books[isbn].reviews[review] = review;
+
+    return res.status(200).json({ message: "Review added/modified successfully.", reviews: books[isbn].reviews });
 });
 
 module.exports.authenticated = regd_users;
