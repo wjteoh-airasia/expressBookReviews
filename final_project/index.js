568f4dd243bec547e9382577de91fe52b92a2e56
Alejandro Santoyo <v-asantoyo@expediagroup.com>
2024-10-28 21:33:56 -0600

    Final Project

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+const { JWT_SECRET } = require('./utils/constants.js');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -8,15 +9,35 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", (req, res, next) => {
+    try {
+        const { authorization } = req.session;
+        if (authorization) {
+            const token = authorization["accessToken"];
+
+            // Verify
+            jwt.verify(token, JWT_SECRET, (err, user) => {
+                if (!err) {
+                    req.user = user;
+                    next();
+                } else {
+                    return res.status(403).json({ message: "User not authenticated", error: err });
+                }
+            });
+        } else {
+            return res.status(403).json({ message: "User not logged in" });
+        }
+    } catch (error) {
+        console.error("error", error)
+        res.status(500).json({ error })
+    }
 });
- 
-const PORT =5000;
+
+const PORT = process.env.PORT || 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,83 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
+const { JWT_SECRET } = require('../utils/constants.js');
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+  return !users.some(user => user.username === username);
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+  return users.some(user => user.username === username && user.password === password);
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Error ligging in! Missing username / password" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    const payload = {
+      username,
+    };
+
+    const accessToken = jwt.sign(payload, JWT_SECRET, { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+      accessToken, username
+    };
+
+    return res.status(200).json({ message: "User successfully logged in" });
+  }
+
+  return res.status(400).json({ message: "User not registered!" });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username } = req.user;
+  const { isbn } = req.params;
+  const { review } = req.body;
+
+  if (!review) {
+    return res.status(400).json({ message: "No review in body" });
+  }
+
+  const book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  book.reviews = {
+    ...book.reviews,
+    [username]: {
+      review
+    }
+  };
+
+  return res.status(200).json(book);
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const { isbn } = req.params;
+  const { username } = req.user;
+  const book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  delete book.reviews[username];
+
+  return res.status(200).json({ book });
 });
 
 module.exports.authenticated = regd_users;
