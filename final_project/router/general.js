e6463c185b74223bfea76852e1ef1e5c4760877e
Amen-Zelealem <amenzelealemtadese@gmail.com>
2025-01-06 01:33:15 -0500

    Complete Task 5: Add endpoint to get book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -54,8 +54,14 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const { isbn } = req.params; // Get the ISBN from the request parameters
+    const book = books[isbn]; // Find the book by ISBN
+
+    if (book && book.reviews) {
+        return res.status(200).json(book.reviews); // Return the reviews
+    } else {
+        return res.status(404).json({ message: "Reviews not found for this book" }); // No reviews
+    }
 });
 
 module.exports.general = public_users;
