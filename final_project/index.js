7d5015ab1c81ade3365e78484ab62c12700d9a9a
joana faria <paisdefaria.joana@gmail.com>
2025-01-15 14:40:54 +0000

    testing problems with push command

--- final_project/index.js
+++ final_project/index.js
@@ -15,13 +15,13 @@ app.use("/customer/auth/*", function auth(req,res,next){
     let token = req.session.authorization['accessToken'];
 
     jwt.verify(token, "access", (err, user) => {
-        if (!err) {
-            req.user = user;
-            next();
-        } else {
-            return res.status(403).json({ message: "User not authenticated" });
-        }
-    });
+      if (!err) {
+        req.user = user;
+        next();
+      } else {
+        return res.status(403).json({ message: "User not authenticated" });
+      }
+  });
 
   } else {
     return res.status(403).json({ message: "User not logged in" });
