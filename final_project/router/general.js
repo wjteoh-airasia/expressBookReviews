3c2f426c0c5ff1be3641e63ce849a2db09cd4457
nicollesenam <nicollesenam@gmail.com>
2025-01-19 18:45:30 -0400

    Adding 10 to 13 tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,39 +27,51 @@ const isUserExistent = (username) => {
 }
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).json(books);
+public_users.get('/', async function (req, res) {
+  try {
+    const api = await axios.get("url_api_here");
+    const books = api.data;
+    return res.send(200).json(books);
+  } catch (error) {
+    return res.status(500).json({"message": "There was an error fetching your request."});
+  }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', async function (req, res) {
   const isbnParam = req.params.isbn;
 
-  for (let id in books) {
-    if (books[id].isbn === isbnParam) {
-      return res.status(200).json(books[id]);
-    }
+  try {
+    const response = await axios.get(`url_api_here/${isbnParam}`);
+    const book = response.data;
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching book details."});
   }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   const authorSearched = req.params.author;
-  for (let id in books) {
-    if (books[id].author.toLowerCase().includes(authorSearched.toLowerCase())) {
-      return res.status(200).json(books[id]);
-    }
+
+  try {
+    const response = await axios.get(`url_api_here/${authorSearched}`);
+    const author = response.data;
+    res.status(200).json(author);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching author details."});
   }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   const titleSearched = req.params.title;
-
-  for (let id in books) {
-    if (books[id].title.toLowerCase().includes(titleSearched.toLowerCase())) {
-      return res.status(200).json(books[id]);
-    }
+  try {
+      const response = await axios.get(`URL_TO_GET_BOOKS_BY_TITLE/${titleSearched}`);
+      const title = response.data;
+      res.status(200).json(title);
+  } catch (error) {
+      res.status(500).json({ message: "Error fetching books by title."});
   }
 });
 
