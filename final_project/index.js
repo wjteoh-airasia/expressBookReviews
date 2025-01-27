a86d6f059f4a92b07eb999d5847bf063495a9eb2
Rocco Twardzik <r.twardzik22@gmail.com>
2025-01-27 18:02:52 -0500

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
- if (req.session.authorization) {
+//Write the authenication mechanism here
 });
  
 const PORT =5000;
