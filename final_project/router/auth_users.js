8668e6aaf37e1747d35b0068a878bd789d00182b
Navaneeth Gade <navaneethgade07@gmail.com>
2025-02-12 23:40:37 +0530

    Task-8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -53,11 +53,29 @@ regd_users.post("/login", (req,res) => {
 
 });
 
-// Add a book review
+// Add a book review or modify
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const {review} = req.query;
+  const isbn = req.params.isbn;
+  const username = req.session.authorization['username'];
+
+  if(!review){
+    return res.status(400).json({message:'Review is required'});
+  }
+
+  if(!books[isbn]){
+    return res.status(404).json({message:'Book not found'});
+  }
+
+  books[isbn].reviews[username] = review;
+
+  return res.status(200).json({message:'Book Review added or updated successfully', reviews: books[isbn].reviews});
+
 });
+
+
+
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
