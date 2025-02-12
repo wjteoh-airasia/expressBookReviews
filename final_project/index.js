bf186f008f4559108a9a354388949d52e3930971
Navaneeth Gade <navaneethgade07@gmail.com>
2025-02-12 12:23:48 +0530

    Authentication Mechanism

--- final_project/index.js
+++ final_project/index.js
@@ -8,13 +8,34 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",session(
+    {
+        secret:"fingerprint_customer",
+        resave: true, 
+        saveUninitialized: true}
+));
 
 app.use("/customer/auth/*", function auth(req,res,next){
+
 //Write the authenication mechanism here
+    if(req.session.authorization){
+        let token = req.session.authorization['accessToken'];
+
+        // verify the token
+        jwt.verify(token, 'access' , (err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            return res.status(403).json({message:'User is not authenticated'});
+        });
+        
+    } else {
+        return res.status(403).json({message: 'User not logged in'});
+    }
 });
  
-const PORT =5000;
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
