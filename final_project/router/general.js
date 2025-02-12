dc5879f719180441544334c667392b8ed07a4426
Navaneeth Gade <navaneethgade07@gmail.com>
2025-02-12 13:46:28 +0530

    Task-2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,11 +17,17 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get("/isbn/:isbn", function (req, res) {
+  const isbn = req.params.isbn;
+
+  if(books[isbn]){
+    res.send(books[isbn]);
+  } else{
+    return res.status(404).json({message:'Book not found'});
+  }
   
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
