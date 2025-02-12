a64fb1504cb8e5d065ed763d3da3f01a10eadb63
Navaneeth Gade <navaneethgade07@gmail.com>
2025-02-12 22:08:17 +0530

    Task-7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -20,12 +20,37 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
-}
+  let validateUser = users.filter(user => {
+    return (user.username == username && user.password === password);
+  });
+  if(validateUser.length > 0){
+    return true;
+  } else{
+    return false;
+  }
+};
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const {username,password} = req.body;
+
+  if(!username || !password){
+    return res.status(404).json({message:'Error loggin in'});
+  }
+
+  if(authenticatedUser(username,password)){
+    // Generate the JWT Token
+    const accessToken = jwt.sign({data:password}, 'access', {expiresIn: 60 * 60});
+
+    req.session.authorization = {
+      accessToken,
+      username
+    }
+    return res.status(200).json({message:'User logged in successfully'});
+  } else {
+    return res.status(208).json({message:'Invalid Login. Check username and password or Register'})
+  }
+
 });
 
 // Add a book review
