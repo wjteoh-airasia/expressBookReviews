5486248fabb9e14c6e63e0adcdbec56a8990c1a6
Navaneeth Gade <navaneethgade07@gmail.com>
2025-02-12 15:38:57 +0530

    Task-5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -54,8 +54,15 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if(books[isbn]){
+    const bookDetails = books[isbn];
+    const review = bookDetails.reviews;
+    return res.status(200).json(review);
+  }
+  else{
+    return res.status(404).json({message:'Reviews not found for this book'});
+  }
 });
 
 module.exports.general = public_users;
