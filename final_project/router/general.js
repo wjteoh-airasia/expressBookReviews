e6f3f99ecc2af47c02f369eae9bc56d673f41fe4
Navaneeth Gade <navaneethgade07@gmail.com>
2025-02-13 15:48:56 +0530

    Task-11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -48,16 +48,40 @@ public_users.get('/' , async (req,res) => {
 
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+// public_users.get("/isbn/:isbn", function (req, res) {
+//   const isbn = req.params.isbn;
+
+//   if(books[isbn]){
+//     res.send(books[isbn]);
+//   } else{
+//     return res.status(404).json({message:'Book not found'});
+//   }
+  
+// });
+
+
+// Task-11
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', async (req,res) => {
   const isbn = req.params.isbn;
 
-  if(books[isbn]){
-    res.send(books[isbn]);
-  } else{
-    return res.status(404).json({message:'Book not found'});
-  }
-  
+  new Promise((resolve,reject) => {
+    const book = books[isbn];
+    if(book){
+      resolve(book);
+    } else {
+      reject('Book not found');
+    }
+  })
+  .then((book) => {
+    res.status(200).json(book)
+  })
+  .catch((error)=>{
+    res.status(404).json({message:error})
+  })
+
 });
+
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
