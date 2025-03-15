bfeeac7cd30a506080cb2c39c1c98f886f327d51
Ibrahim Younes Zaidi <zaidibrahim2003@Gmail.com>
2025-03-16 00:28:52 +0100

    initial commit

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,30 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const general_routes = require("./router/general.js").general;
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  //Write the authenication mechanism here
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
-app.use("/", genl_routes);
+app.use("/", general_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,87 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/", function (req, res) {
+  res.status(200).send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/isbn/:isbn", function (req, res) {
+  const isbnID = req.params.isbn;
+
+  if (books[isbnID]) {
+    return res.status(200).send(JSON.stringify(books[isbnID]));
+  } else {
+    return res
+      .status(404)
+      .send(JSON.stringify({ message: "there is no book for this code" }));
+  }
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Get book details based on author
+public_users.get("/author/:author", function (req, res) {
+  const author = req.params.author.toLocaleLowerCase();
+  const bookByAuthor = {};
+
+  for (const [ID, bookObj] of Object.entries(books)) {
+    let book_author = bookObj.author.toLocaleLowerCase();
+    let first_name_book_author = bookObj.author
+      .split(" ")
+      .at(0)
+      .toLocaleLowerCase();
+
+    if (book_author === author || first_name_book_author === author) {
+      bookByAuthor[ID] = bookObj;
+    }
+  }
+
+  if (Object.keys(bookByAuthor).length > 0) {
+    return res.status(200).send(JSON.stringify(bookByAuthor));
+  }
+
+  return res.status(404).json({ message: "there is no book for this author" });
+});
+
+public_users.get("/title/:title", function (req, res) {
+  const title = req.params.title;
+  const bookByTitle = {};
+
+  for (const [ID, bookObj] of Object.entries(books)) {
+    if (bookObj.title.toLocaleLowerCase() === title.toLocaleLowerCase()) {
+      bookByTitle[ID] = bookObj;
+    }
+  }
+
+  if (Object.keys(bookByTitle).length === 0) {
+    return res
+      .status(404)
+      .json({ message: "there are no books by this title" });
+  }
+  return res.status(200).send(JSON.stringify(bookByTitle));
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/review/:isbn", function (req, res) {
+  const bookID = req.params.review;
+  const book_Obj_Review = {};
+
+  for (const [ID, book] of Object.entries(books)) {
+    if (bookID === ID) book_Obj_Review[ID] = book.reviews;
+  }
+
+  console.log(book_Obj_Review);
+
+  return res.status(200).send(JSON.stringify(book_Obj_Review));
 });
 
 module.exports.general = public_users;
