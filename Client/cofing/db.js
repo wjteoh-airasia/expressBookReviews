979411da4de728e4af79a8d4ac82a3f2aefdf67e
Ibrahim Younes Zaidi <zaidibrahim2003@Gmail.com>
2025-03-25 07:36:56 +0100

    updated env variables

--- Client/cofing/db.js
+++ Client/cofing/db.js
@@ -1,10 +1,16 @@
 import { Sequelize } from "sequelize";
+import "dotenv/config";
 
-export const sequelize = new Sequelize("library", "postgres", "bourousia", {
-  host: "localhost",
-  port: 5432,
-  dialect: "postgres",
-});
+export const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: "postgres",
+  }
+);
 
 try {
   await sequelize.authenticate();
@@ -14,27 +20,3 @@ try {
 }
 
 export default sequelize;
-
-// let books = {
-//   1: { author: "Chinua Achebe", title: "Things Fall Apart", reviews: {
-// } },
-//   2: { author: "Hans Christian Andersen", title: "Fairy tales", reviews: {} },
-//   3: { author: "Dante Alighieri", title: "The Divine Comedy", reviews: {} },
-//   4: { author: "Unknown", title: "The Epic Of Gilgamesh", reviews: {} },
-//   5: { author: "Unknown", title: "The Book Of Job", reviews: {} },
-//   6: { author: "Unknown", title: "One Thousand and One Nights", reviews: {} },
-//   7: { author: "Unknown", title: "Nj\u00e1l's Saga", reviews: {} },
-//   8: { author: "Jane Austen", title: "Pride and Prejudice", reviews: {} },
-//   9: {
-//     author: "Honor\u00e9 de Balzac",
-//     title: "Le P\u00e8re Goriot",
-//     reviews: {},
-//   },
-//   10: {
-//     author: "Samuel Beckett",
-//     title: "Molloy, Malone Dies, The Unnamable, the trilogy",
-//     reviews: {},
-//   },
-// };
-
-// export default books;
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-- This is a basic CRUD application with authentication using both sessions and JWT.
+- This is a CRUD application with authentication using JWT authentication.
 
-- I used Node/Express for this application
+- I used Node/Express for this application.
+
+- I used postgreSQL as a database of the application, with sequeliez as an ORM.
