0e6fa4a1a48cf6dbda6396109ec1783c6d554e96
Shivani Bodara <shivanibodara1@gmail.com>
2025-04-03 13:11:54 -0400

    Implemented get methods

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,21 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null,4));
+  return res.status(300).json({message: ""});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let getisbn = req.params.isbn;
+  let book = books[getisbn];
+  if(book) {
+    res.json(book);
+  }
+  else {
+    res.status(404).json({message: "Book not found"});
+  }
+//   return res.status(300).json({message:""});
  });
   
 // Get book details based on author
