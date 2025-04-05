02da0aec18ea45478ed92508fc12e22f2d0ee6ec
therohit15 <rohitk15102004@gmail.com>
2025-04-05 12:16:04 -0400

    task 2&3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,19 +12,24 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books))
+    res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    let isbn = req.params.isbn;
+    res.send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let author = req.params.author;
+    let details = Object.values(books).filter((book)=> book.author===author);
+    if(details.length>0){
+        res.status(200).json(details);
+    }else{
+        res.status(404).json({message:"author not found"})
+    }
 });
 
 // Get all books based on title
