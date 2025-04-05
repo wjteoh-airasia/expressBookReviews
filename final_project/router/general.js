5902da0fcd1024be09010a736d9e2039e599041b
therohit15 <rohitk15102004@gmail.com>
2025-04-05 12:28:04 -0400

    task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,13 +12,17 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books));
+    res.status(200).send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     let isbn = req.params.isbn;
-    res.send(books[isbn]);
+    if(books[isbn]){
+        res.status(200).json(books[isbn]);
+    }else{
+        res.status(404).json({message:"author not found"});
+    }
 });
   
 // Get book details based on author
@@ -28,14 +32,19 @@ public_users.get('/author/:author',function (req, res) {
     if(details.length>0){
         res.status(200).json(details);
     }else{
-        res.status(404).json({message:"author not found"})
+        res.status(404).json({message:"author not found"});
     }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let title = req.params.title;
+    let details = Object.values(books).filter((book)=> book.title===title);
+    if(details.length>0){
+        res.status(200).json(details);
+    }else{
+        res.status(404).json({message:"title not found"});
+    }
 });
 
 //  Get book review
