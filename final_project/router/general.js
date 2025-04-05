027d060c60bd11fbd4599723273d5eee99c53238
therohit15 <rohitk15102004@gmail.com>
2025-04-05 15:13:17 -0400

    task 12

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -62,13 +62,23 @@ public_users.get('/isbn/:isbn',async function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/books/author/:author',function (req, res) {
     let author = req.params.author;
     let details = Object.values(books).filter((book)=> book.author===author);
     if(details.length>0){
         res.status(200).json(details);
     }else{
         res.status(404).json({message:"author not found"});
+    }
+});
+
+public_users.get('/author/:author',async function (req, res) {
+    try {
+        const author = req.params.author;
+        const books = await axios.get(`https://rohitk151020-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/books/author/${author}`);
+        res.status(200).json(books.data);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching author", error: error.message });
     }
 });
 
