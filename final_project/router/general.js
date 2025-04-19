3a02e65c34240a5db7bf4c973b75054aef91cf53
Amr Mostafa <amrmos.it@gmail.com>
2025-04-19 21:19:58 +1000

    Completing the code for getting the book details based on ISBN under public_users.get('/isbn/:isbn',function (req, res) {}

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,15 +12,27 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send(JASON.stringfy({books}), null, 4)
+  res.status(200).send(JSON.stringify({books}), null, 4);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Extract the isbn parameter from the request parameters
+  const isbn = req.params.isbn;
+  // Access the book using the ISBN key
+  const book = books[isbn];
+  if (book){
+    //if the book exists, send it as the response
+    res.status(200).send(JSON.stringify(book, null, 4));
+  } else {
+    res.status(404).json({message: "Book not found"});
+  } 
  });
   
+
+
+
+ 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
