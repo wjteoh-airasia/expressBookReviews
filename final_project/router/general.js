975aad84c9f65725c37be0af3c981ec0b6c4cd59
Mariia <masha.shmidt.04@gmail.com>
2025-07-30 22:23:16 -0400

    registration handled

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,8 +7,26 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  
-return res.send})
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // 1. Check if both username and password are provided
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  // 2. Check if user already exists
+  const userExists = users.some((user) => user.username === username);
+
+  if (userExists) {
+    return res.status(409).json({ message: "Username already exists" });
+  }
+
+  // 3. Add user to array
+  users.push({ username, password });
+
+  return res.status(200).json({ message: "User successfully registered. Now you can login." });
+  })
 
 // Get the book list available in the shop
 public_users.get('/', (req, res) => {
@@ -85,10 +103,20 @@ public_users.get('/title/:title',function (req, res) {
   }
 });
 
+
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const isbn = req.params.isbn;
+  const book = books[isbn]
+  const reviews = book.reviews;
+  if (reviews){
+    return res.status(200).json({
+        message: `Your book reviews for book ${book.title}: `,
+        reviews: book.reviews
+      });
+  }else{
+  return res.status(404).json({message: "No reviews found"});
+}});
 
 module.exports.general = public_users;
