050e2cbf3af8a336df8b640c9e3eca246e436fc5
ibrahimkewan0 <ibrahimkewan@outlook.de>
2025-08-07 11:47:49 +0200

    änderungen an struktur statt normal send wird alles als json gesendet

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,70 +1,88 @@
 const express = require("express");
 let books = require("./booksdb.js");
-const { parse } = require("dotenv");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
     const { username, password } = req.body;
-
     if (!username || !password) {
-        return res.send(`Benutzer name und passwort sind erforderlich`);
+        return res.status(400).json({
+            message: "Benutzer name und password sind pflicht angaben!!",
+        });
     }
 
-    const existingUser = users.find((user) => user.username === username);
-    if (existingUser) {
-        return res.send(`Diese Benutzer ${username} exister schon bereit`);
+    const isExist = users.find((user) => user.username === username);
+    if (isExist) {
+        return res.status(400).json({
+            message: "Diese Benutzer ist schon bereits existiert",
+        });
     }
-    users.push({ username, password });
-    return res.status(201).json({
-        message: `Benutzer ${username} wurde erfolgreich registriert`,
+    users.push({
+        username,
+        password,
     });
-});
-
-public_users.get("/users", (req, res) => {
-    res.json(users);
+    return res.status(201).json({
+        message: "Benutzer wurde erffolgreich registriert :)",
+        username: username,
+    });
 });
 
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
-    res.status(200).send(JSON.stringify(books));
+    return res.status(200).json({
+        message: "Die Bücher liste: ",
+        books,
+    });
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-    const ISBN = parseInt(req.params.isbn);
-    const findeISBN = books[ISBN];
-    if (!findeISBN) {
-        return res.status(404).send("Book not found");
+    const isbn = parseInt(req.params.isbn);
+    const book = books[isbn];
+    if (!book) {
+        return res.status(404).json({
+            message: "Buch nicht gefunden",
+        });
     }
-    return res.status(200).json(findeISBN);
+    return res.status(201).json({
+        message: "Buch Details: ",
+        book: book,
+    });
 });
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-    const author = req.params.author.toLocaleLowerCase();
-    const book = Object.values(books).find(
-        (b) => b.author.toLocaleLowerCase() === author
+    const autor = req.params.author.toLocaleLowerCase();
+    const findAutor = Object.values(books).filter(
+        (book) => book.author.toLocaleLowerCase() === autor
     );
-    if (!book) {
-        return res
-            .status(404)
-            .send("Es gibt kein bücher für diese Author bei uns !!! ");
+    if (findAutor.length === 0) {
+        return res.status(404).json({
+            message: "Autor nicht gefunden",
+        });
     }
-    return res.status(200).json(book);
+    return res.status(200).json({
+        message: `Diese autor ${autor} hat diese buch`,
+        findAutor,
+    });
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-    const title = req.params.title.toLocaleLowerCase();
-    const book = Object.values(books).find(
-        (t) => t.title.toLocaleLowerCase() === title
+    const title = req.params.title;
+    const book = Object.values(books).filter(
+        (b) => b.title.toLocaleLowerCase() === title.toLocaleLowerCase()
     );
-    if (!book) {
-        return res.status(404).send("Buch mit diesem Title gibt es nicht ");
+    if (book.length === 0) {
+        return res.status(404).json({
+            message: "buch mit diesem Title nicht gefunden",
+        });
     }
-    return res.status(200).json(book);
+    return res.status(200).json({
+        title: title,
+        books: book,
+    });
 });
 
 //  Get book review
@@ -72,9 +90,21 @@ public_users.get("/review/:isbn", function (req, res) {
     const isbn = parseInt(req.params.isbn);
     const book = books[isbn];
     if (!book) {
-        return res.send("Buch nicht gefunden");
+        return res.status(404).json({
+            message: "buch nicht gefunden",
+        });
     }
-    return res.status(200).send(book.reviews);
+
+    const reviewKeys = Object.keys(book.reviews);
+    if (reviewKeys.length === 0) {
+        return res.json({
+            message: "diese buch hat kein bewertung",
+        });
+    }
+    return res.json({
+        message: "Bewertung",
+        reviews: book.reviews,
+    });
 });
 
 module.exports.general = public_users;
